ae72794dcc865a2253ba64d7dfbf27100183f202
Скопин Михаил <skom@omegafututre.ru>
2024-09-20 14:06:06 +0300

    init

---
+++ yandex-postbox-test/Program.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+
+var awsAccessKeyId = "";
+var awsSecretAccessKey = "";
+var postboxUrl = "https://postbox.cloud.yandex.net";
+
+var toAddresses = "";
+var sourceEmail = "";
+
+
+using var client = new AmazonSimpleEmailServiceClient(awsAccessKeyId, awsSecretAccessKey, new AmazonSimpleEmailServiceConfig
+{
+    ServiceURL = postboxUrl
+});
+
+var response = await client.SendEmailAsync(new SendEmailRequest
+{
+    Destination = new Destination([toAddresses]),
+    Message = new Message(new Content("Тема"), new Body(new Content("Hello World!"))),
+    Source = sourceEmail,
+});
