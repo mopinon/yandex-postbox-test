97f7405dbb22a66e75ddb669bb773fcae4ca84bf
Скопин Михаил <skom@omegafututre.ru>
2024-09-29 21:51:44 +0300

    to v2 sdk

--- yandex-postbox-test/Program.cs
+++ yandex-postbox-test/Program.cs
@@ -1,5 +1,5 @@
-﻿using Amazon.SimpleEmail;
-using Amazon.SimpleEmail.Model;
+﻿using Amazon.SimpleEmailV2;
+using Amazon.SimpleEmailV2.Model;
 
 var awsAccessKeyId = "";
 var awsSecretAccessKey = "";
@@ -9,14 +9,41 @@ var toAddresses = "";
 var sourceEmail = "";
 
 
-using var client = new AmazonSimpleEmailServiceClient(awsAccessKeyId, awsSecretAccessKey, new AmazonSimpleEmailServiceConfig
+using var client = new AmazonSimpleEmailServiceV2Client(awsAccessKeyId, awsSecretAccessKey, new AmazonSimpleEmailServiceV2Config()
 {
-    ServiceURL = postboxUrl
+    ServiceURL = postboxUrl,
+    AuthenticationRegion = "ru-central1"
+
 });
 
 var response = await client.SendEmailAsync(new SendEmailRequest
 {
-    Destination = new Destination([toAddresses]),
-    Message = new Message(new Content("Тема"), new Body(new Content("Hello World!"))),
-    Source = sourceEmail,
+    Destination = new Destination
+    {
+        ToAddresses = [toAddresses],
+    },
+    Content = new EmailContent
+    {
+        Raw = null,
+        Simple = new Message
+        {
+            Subject = new Content
+            {
+                Data = "Тема"
+            },
+            Body = new Body
+            {
+                Text = new Content
+                {
+                    Data = "Ваш код: 1234"
+                }
+            },
+        }
+    },
+    FromEmailAddress = sourceEmail,
 });
+
+
+
+
+
